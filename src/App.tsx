51ef636e1931ca9783cd108b2cf4898978af74c6
Pedro Oliveira <65639491+PedroUNB@users.noreply.github.com>
2021-01-04 16:29:30 -0300

    Create the balloon component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import { Chip } from "./components/Chip/Chip";
 import { Card } from "./components/Card/Card";
+import { Balloon } from "./components/Balloon/Balloon";
 import "./styles.css";
 
 export default function App() {
@@ -17,6 +18,13 @@ export default function App() {
         <div className="textBox">
           <span className="title">Card:</span>
           <Card />
+        </div>
+      </div>
+
+      <div className="section">
+        <div className="textBox">
+          <span className="title">Card:</span>
+          <Balloon />
         </div>
       </div>
     </div>
---
+++ src/components/Balloon/Balloon.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./balloon-style.css";
+
+export function Balloon() {
+  return (
+    <div className="balloon">
+      <p>Olá 2</p>
+    </div>
+  );
+}
